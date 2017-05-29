97ebb1049a9df8744ad224c1e24df1ece2f4ad17
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 13:07:16 -0700

    added function and func and for loop

--- wordcount.py
+++ wordcount.py
@@ -8,6 +8,16 @@ our_file = open("test.txt")
 #we want the function to take the body of text inside file and counts how many times 
 #each word occurs separated by spaces
 #we need a empty dictionary to store word count
+# create for loop for file;
 #we need a for loop that iterates thru file 
 #inside our for loop we need a conditional that checks if word is in dict then +1 if not add 1
 
+def count_words(afile):
+    our_dict = {}
+    #sentence = afile.split(" ")
+    
+    for line in afile:
+        sentence = line.split()
+        print sentence
+
+count_words(our_file)
