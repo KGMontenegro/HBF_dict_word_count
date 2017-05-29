fadfa9a1cbe0f7b6c84e2e5e8809897b79574e52
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 12:43:22 -0700

    intial commit and added pseudo code and opened file

---
+++ wordcount.py
@@ -0,0 +1,13 @@
+# put your code here.
+our_file = open("test.txt")
+#our_bigger_file =open("twain.txt")
+
+#pseudo code
+#open file
+#create a function
+#we want the function to take the body of text inside file and counts how many times 
+#each word occurs separated by spaces
+#we need a empty dictionary to store word count
+#we need a for loop that iterates thru file 
+#inside our for loop we need a conditional that checks if word is in dict then +1 if not add 1
+
